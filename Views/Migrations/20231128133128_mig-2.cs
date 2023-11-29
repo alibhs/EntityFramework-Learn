9c640dac08d225f444689bd986159706571e22c0
alibhs <alibahsi772@gmail.com>
2023-11-30 00:26:02 +0300

    View, Stored Procedure and Scalar

---
+++ Views/Migrations/20231128133128_mig-2.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Views.Migrations
+{
+    public partial class mig2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"
+            Create View vm_PersonOrders
+            As
+            Select p.Name , COUNT(*) [Count] from Persons p
+            Inner join Orders o on p.personId  = o.PersonId
+            Group By p.Name
+            order by [Count] Desc
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql($@"Drop View vm_PersonOrders");
+        }
+    }
+}
